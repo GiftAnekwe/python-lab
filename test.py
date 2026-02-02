b5c5d2e47ca4b34344c3291c0c67f31a4a14f72b
Anekwe, Gift Adaeze <giftanekwe@outlook.com>
2026-02-02 20:07:29 +0100

    Fix quadratic formula calculation and output format

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ print("Now, let's calculate another... hehehe")
 A = float(input("Give 'A' a number: "))
 B = float(input("Give 'B' a number: "))
 C = float(input("Give 'C' a number: "))
-x_plus = (-B + ((B**2) - (4*A*C)))/2*A
-x_minus = (-B - ((B**2) - (4*A*C)))/2*A
+x_plus = (-B + ((B**2) - (4*A*C))**0.5)/(2*A)
+x_minus = (-B - ((B**2) - (4*A*C))**0.5)/(2*A)
 print('Using the Quadratic formula, X = ', x_plus, 'or', x_minus)
 print('                                ')
 print("Experimenting our acquired skills in currency exchange...")
@@ -24,8 +24,8 @@ soles = float(input('How much worth of soles remains? '))
 soles_usd = soles*0.30
 reais = float(input('How much worth of reais remains? '))
 reais_usd = reais*0.19
-USD_Total = int(pesos_usd + soles_usd + reais_usd)
-print("Your total amount of leftover cash is approximately",USD_Total,"USD.")
+USD_Total = pesos_usd + soles_usd + reais_usd
+print("Your total amount of leftover cash is",USD_Total,"USD.")
 print("                                  ")
 
 #Debugging demo
